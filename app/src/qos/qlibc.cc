ff5c858377988a288b7b5a821b33cce36a72037b
hlqqlm <hlqqlm@qq.com>
2023-09-12 17:00:10 +0800

    nobase_include_HEADERS is created by patsubst function in Makefile.am.

---
+++ app/src/qos/qlibc.cc
@@ -0,0 +1,26 @@
+/*
+This program is licensed under the GPLv2 License:
+Copyright 2023 Huanglin <hlqqlm@qq.com>
+
+
+This file is part of Split698.
+
+Split698 is free software: you can redistribute it and/or modify it under the
+terms of the GNU General Public License as published by the Free Software
+Foundation, either version 2 of the License, or (at your option) any later
+version.
+
+Split698 is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+Split698. If not, see <https://www.gnu.org/licenses/>.
+
+
+20230912------------------------------------------------------------------------
+created.
+*/
+#include "qlibc.h"
+#include "qlibc.xcp.h"
+
